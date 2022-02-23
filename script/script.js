94754936d02d326e21dd3d838e14188ca70998ec
haniframadhani <mhaniframadhani985@gmail.com>
2022-02-23 10:01:06 +0800

    ubah-ubah icon

--- script/script.js
+++ script/script.js
@@ -3,10 +3,35 @@ feather.replace();
 
 // my script
 const searchBar = document.querySelector('.search-bar');
-const iconNav = document.querySelector('.icon-nav');
+const iconNav = document.querySelectorAll('.icon-nav');
 const searchBtn = document.querySelector('.btn-cari');
+const inputSpesifik = document.getElementById('input-spesifik');
 
-searchBar.addEventListener('click', function () { console.log('button cari') });
-iconNav.addEventListener('click', function () {
-    searchBar.classList.toggle('tutup-search');
+// input.oninput = e => {
+// 	const value = e.target.value
+// 	if (value) e.target.value = e.target.value.substring(0, 1)
+// }
+
+function clickEvent(first, last) {
+	if (first.value.lenght) {
+		document.getElementById(last).focus();
+	}
+}
+
+inputSpesifik.addEventListener('click', function () {
+	console.log(inputSpesifik);
+});
+
+searchBtn.addEventListener('click', function () {
+	searchBar.classList.toggle('tutup-search');
+});
+
+iconNav.forEach((icon, i) => {
+	icon.onclick = () => {
+		let next = i + 1
+		if (next >= iconNav.length) next = 0;
+		icon.style.display = "none";
+		iconNav[next].style.display = "block";
+		searchBar.classList.toggle('tutup-search');
+	}
 });
